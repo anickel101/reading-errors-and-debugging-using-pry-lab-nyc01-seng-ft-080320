77b745e3b189c39040df1f8068c7c5d039f111a4
Anson Nickel <anson.nickel@gmail.com>
2020-07-14 01:01:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,16 +2,9 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    s = "s"
-    10 * s + string
-    binding.pry
     string.prepend("ssssssssss")
-    binding.pry
+
   else
     string
   end
 end
-
-seven = "seven"
-
-snake_it_up(seven)
